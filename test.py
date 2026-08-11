577f55bf61caa4b7c3674f902a2452a7bba3dc03
Bunleab Chea <bleab771@gmail.com>
2026-08-11 09:34:17 +0700

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-
-import joblib
-model = joblib.load('rf_day1.pkl')
-print('Number of features:', model.n_features_in_)
-print('Feature names:')
-for f in model.feature_names_in_:
-    print(' -', f)
